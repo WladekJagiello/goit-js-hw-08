6adca13fb99aa78af8e4c83cb1488e832f33b4f2
Vadym <120068188+WladekJagiello@users.noreply.github.com>
2023-05-10 23:13:15 +0300

    ...

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const video = document.querySelector('iframe');
+const player = new Player(video);
+const updateCurrentTime = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(updateCurrentTime, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
