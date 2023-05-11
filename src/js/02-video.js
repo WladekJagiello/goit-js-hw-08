2abfc3fdab5d1cf896cf1754e8a119213c80b468
Vadym <120068188+WladekJagiello@users.noreply.github.com>
2023-05-11 22:38:51 +0300

    ...

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,8 @@ import throttle from 'lodash.throttle';
 
 const video = document.querySelector('iframe');
 const player = new Player(video);
-const updateCurrentTime = data => {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+const updateCurrentTime = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
 };
 
 player.on('timeupdate', throttle(updateCurrentTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
 import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('[name="email"]');
+const messageEl = document.querySelector('[name="message"]');
+let feedbackFormState = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+) || { email: '', message: '' };
+
+const savedData = () => {
+  emailEl.value = feedbackFormState.email || '';
+  messageEl.value = feedbackFormState.message || '';
+};
+
+savedData();
+
+formEl.addEventListener(
+  'input',
+  throttle(event => {
+    feedbackFormState[event.target.name] = event.target.value;
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(feedbackFormState)
+    );
+  }, 500)
+);
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(feedbackFormState);
+  event.currentTarget.reset();
+  feedbackFormState = { email: '', message: '' };
+});
